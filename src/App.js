138fd7ceb074d172bf71718737cd19caf88c7114
Ali-vakili <ali.vakili.tb@gmail.com>
2023-02-10 17:33:36 +0330

    initial app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;

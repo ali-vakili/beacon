3f9171a5ca9b9448921b669a9b765a20cb35ae41
ali-vakili <ali.vakili.tb@gmail.com>
2023-02-20 20:10:37 +0330

    create getFetch.jsx and use it in the Market.jsx to get data from api make coing.jsx to render dynamic data and fromat them correctly change some basic styles

--- src/pages/market/Market.jsx
+++ src/pages/market/Market.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import "./market.css";
+import getFetch from "../../lib/getFetch";
 import Title from "../../components/ui/title";
 import Indicator from "../../components/ui/indicator";
 import Skeleton from "../../components/ui/skeleton";
 import Coin from "./coin";
 
 class Market extends Component {
-  state = {};
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  state = {
+    page: 1,
+    coins: null,
+    isLoading: true,
+  };
+
+  fetchData = async () => {
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${this.state.page}&sparkline=false&price_change_percentage=1h`;
+    try {
+      this.setState({ isLoading: true });
+      const coins = await getFetch(url);
+      this.setState({ coins, isLoading: false });
+    } catch (error) {
+      this.setState({ error, isLoading: false });
+    }
+  }
+
   render() {
+    const { coins, isLoading, error } = this.state;
     return (
       <section className="market section-beacon">
         <div className="market-container row gx-0">
@@ -16,10 +39,14 @@ class Market extends Component {
             description={"Today's Cryptocurrency Prices"}
           />
           <Indicator />
-          <div className="coin-container indicator-beacon">
-            <Coin />
-          </div>
-          <Skeleton />
+          {isLoading && <Skeleton />}
+          {coins && (
+            <div className="coin-container indicator-beacon">
+              {coins.map((coin) => (
+                <Coin key={coin.id} coin={coin} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     );
--- src/pages/market/coin.jsx
+++ src/pages/market/coin.jsx
@@ -3,78 +3,115 @@ import "../../components/assets/css/indicator.css";
 import "./coin.css";
 
 class Coin extends Component {
-  state = {};
 
   render() {
+    const { coin } = this.props;
+    const {
+      name,
+      symbol,
+      image,
+      current_price,
+      market_cap,
+      price_change_percentage_24h,
+      price_change_percentage_1h_in_currency,
+    } = coin;
+
     return (
       <div className="coin items">
         <div className="coin-name item">
-          <img
-            className="coin-image"
-            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
-            alt="bitcoin"
-          />
+          <img className="coin-image" src={image} alt={name} />
           <div className="name">
-            <h5 className="c-name">Bitcoin</h5>
-            <h5 className="c-symbol m-0">BTC</h5>
+            <h5 className="c-name">{name}</h5>
+            <h5 className="c-symbol m-0">{symbol.toUpperCase()}</h5>
           </div>
         </div>
         <div className="coin-price item">
-          <h5>$ 24,689</h5>
+          <h5>$ {this.formatPrice(current_price)}</h5>
         </div>
         <div className="coin-change change-1h item">
           <div className="change">
-            <div className="change-badge positive-badge">
-              <span className="polygon positive">
+            <div
+              className={this.getBageClasses(
+                price_change_percentage_1h_in_currency
+              )}
+            >
+              <span
+                className={this.getPolygonClasses(
+                  price_change_percentage_1h_in_currency
+                )}
+              >
                 <span className="sr-only">Polygon</span>
               </span>
-              <h5>1h Change</h5>
+              <h5>
+                {this.formatPrice(price_change_percentage_1h_in_currency)}%
+              </h5>
             </div>
           </div>
         </div>
         <div className="coin-change change-24h item">
           <div className="change">
-            <div className="change-badge negative-badge">
-              <span className="polygon negative">
+            <div className={this.getBageClasses(price_change_percentage_24h)}>
+              <span
+                className={this.getPolygonClasses(price_change_percentage_24h)}
+              >
                 <span className="sr-only">Polygon</span>
               </span>
-              <h5>1.1%</h5>
+              <h5>{this.formatPrice(price_change_percentage_24h)}%</h5>
             </div>
           </div>
         </div>
         <div className="coin-market-cap item">
-          <h5>$ 476,369,777,725</h5>
+          <h5>$ {this.formatPrice(market_cap)}</h5>
         </div>
       </div>
     );
   }
 
-  formatPrice = (price) => {
+
+  formatPrice = price => {
     const absolutePrice = Math.abs(price);
     const [integer, decimal] = String(absolutePrice).split(".");
     const formattedDecimal = decimal ? decimal.slice(0, 2) : null;
-    const formttedPrice = Number(integer).toLocaleString("en-US");
-    const correctPrice = decimal
-      ? `${formttedPrice}.${formattedDecimal}`
-      : formttedPrice;
+    let formttedPrice = Number(integer).toLocaleString("en-US");
+    if (formttedPrice === "0" && formattedDecimal === null){
+      formttedPrice = "0.00";
+    }
 
+    const correctPrice = decimal ? `${formttedPrice}.${formattedDecimal}` : `${formttedPrice}`;
     return correctPrice;
   };
 
-  getBageClasses = (number) => {
-    let sign = Math.sign(number);
+  getBageClasses = number => {
     let classes = "change-badge ";
-    classes +=
-      sign === 1 ? "positive-badge" : sign === -1 ? "negative-badge" : "zero";
+    const sign = Math.sign(number);
+
+    const result = this.checkZeroPrice(number);
+    result === true ? classes += "zero" : classes += sign === 1 ? "positive-badge" : sign === -1 ? "negative-badge" : "zero";
     return classes;
   };
 
-  getPolygon = (number) => {
-    let sign = Math.sign(number);
+  getPolygonClasses = number => {
     let classes = "polygon ";
-    classes += sign === 1 ? "positive" : sign === -1 ? "negative" : null;
+    const sign = Math.sign(number);
+
+    const result = this.checkZeroPrice(number)
+    result === true ? classes="" : classes += sign === 1 ? "positive" : sign === -1 ? "negative" : null;
     return classes;
   };
+
+  checkZeroPrice = number => {
+    const absoluteNumber = Math.abs(number);
+    const [integer, decimal] = String(absoluteNumber).split(".");
+    const formattedDecimal = decimal ? decimal.slice(0, 2) : null;
+    const correctPrice = decimal ? `${integer}.${formattedDecimal}` : `${integer}`;
+
+    if (correctPrice === "0" || correctPrice=== "0.00") {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
 
 export default Coin;

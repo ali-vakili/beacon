ce327b717c165ec644140560b6c483894464afbc
ali-vakili <ali.vakili.tb@gmail.com>
2023-03-07 00:36:18 +0330

    fix the indicator sticky position wrapped the App.js in BrowserRouter in the index.js change the App.js into functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import { BrowserRouter ,Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Footer from './components/footer';
 import Hero from './pages/hero/Hero';
@@ -7,25 +6,21 @@ import Market from './pages/market/Market';
 import Explore from './pages/explore/Explore';
 import './App.css';
 
-class App extends Component {
-  state = {}
-  render() {
-    return (
-      <React.Fragment>
-        <div className='container-beacon p-0'>
-          <BrowserRouter>
-            <NavBar />
-            <Routes>
-              <Route exact path="/" element={<Hero />} />
-              <Route exact path="/market" element={<Market />} />
-              <Route exact path="/explore" element={<Explore />} />
-            </Routes>
-          </BrowserRouter>
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
+const App = () => {
+  const location = useLocation();
+  const isHeroRoute = location.pathname === '/' ? 'landing-page-beacon' : '';
+
+  return (
+    <div className={`container-beacon p-0 ${isHeroRoute}`}>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="/market" element={<Market />} />
+        <Route exact path="/explore" element={<Explore />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

a14f3bdf1379a1fb8f8adaf8c5e074c9864316d5
ali-vakili <ali.vakili.tb@gmail.com>
2023-03-17 21:39:58 +0330

    fix the position sticky for the coin indicator

--- src/components/ui/indicator.jsx
+++ src/components/ui/indicator.jsx
@@ -1,27 +1,38 @@
+import { useEffect, useRef } from 'react';
+import { FixedElement } from 'react-nice-scroll';
 import "../assets/css/indicator.css";
 
 const Indicator = () => {
+  const fixedEl = useRef();
+
+  useEffect(() => {
+    fixedEl.current.parentElement.classList.add('ns-sticky-element');
+  }, []);
+
   return (
-    <div className="specifier-container indicator-beacon">
-      <div className="indicator items">
-        <div className="coin-name item">
-          <h5>Coin Name</h5>
-        </div>
-        <div className="coin-price item">
-          <h5>Price</h5>
-        </div>
-        <div className="coin-change change-1h item">
-          <h5>1h Change</h5>
-        </div>
-        <div className="coin-change change-24h item">
-          <h5>24h Change</h5>
-        </div>
-        <div className="coin-market-cap item">
-          <h5>Market Cap</h5>
+    <FixedElement top={0}>
+      <div className="specifier-container indicator-beacon" ref={fixedEl}>
+        <div className="indicator items">
+          <div className="coin-name item">
+            <h5>Coin Name</h5>
+          </div>
+          <div className="coin-price item">
+            <h5>Price</h5>
+          </div>
+          <div className="coin-change change-1h item">
+            <h5>1h Change</h5>
+          </div>
+          <div className="coin-change change-24h item">
+            <h5>24h Change</h5>
+          </div>
+          <div className="coin-market-cap item">
+            <h5>Market Cap</h5>
+          </div>
         </div>
       </div>
-    </div>
+    </FixedElement>
   );
+  
 };
 
 export default Indicator;

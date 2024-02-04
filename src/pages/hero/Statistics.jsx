ad91c0dfead67b0d5c768f82bd578c5f6d5537b4
ali-vakili <ali.vakili.tb@gmail.com>
2024-02-04 21:03:54 +0330

    made the statistics section responsive

--- src/pages/hero/Statistics.jsx
+++ src/pages/hero/Statistics.jsx
@@ -1,18 +1,18 @@
 import CountUp from 'react-countup';
+import { useState } from 'react';
 import { motion } from "framer-motion"
 import { statistics } from "../../constants/statistics"
 
 import quote from "./svg/quote.svg"
 
 import "./statistics.css"
-import { useState } from 'react';
 
 const Statistics = () => {
   const [startCounters, setStartCounters] = useState(false);
 
   return (
     <>
-      <img className="mx-auto" width={64} height={64} style={{maxWidth: "100%", height: "auto"}} src={quote} alt="quote" loading="lazy"/>
+      <img className="quote mx-auto" width={64} height={64} style={{maxWidth: "100%", height: "auto"}} src={quote} alt="quote" loading="lazy"/>
       <motion.p 
       initial={{ visibility: "hidden", y: 30, opacity: 0 }}
       whileInView={{ visibility: "visible", y: 0, opacity: 1 }}
@@ -26,10 +26,10 @@ const Statistics = () => {
       >
         Our goal is to equip traders and investors with practical insights, facilitating intelligent trading choices through the most extensive trading assistant in the world.
       </motion.p>
-      <motion.div className="numbers w-100 mb-3" whileInView={() => setStartCounters(true)} viewport={{ once: true }}>
+      <motion.div className="numbers w-100 mb-3 row" whileInView={() => setStartCounters(true)} viewport={{ once: true }}>
         {statistics.map(({ id, subject, start, end, symbol }) => (
           startCounters && (
-            <div className="statistic">
+            <div className="statistic col-md-3 col-6">
               <div className='count-up'>
                   <CountUp className='number' start={start} end={end} duration={4}/>
                   <span className='number'>{symbol}</span>
